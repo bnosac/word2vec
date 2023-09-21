a4d36ba0008d1248b5e5347e292dfee857d9767e
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-09-21 14:43:03 +0900

    Update test

--- tests/train.R
+++ tests/train.R
@@ -7,17 +7,24 @@ toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE)
 lis <- as.list(toks)
 txt <- stringi::stri_c_list(lis, " ")
 
-mod_lis <- word2vec(lis, dim = 15, iter = 20, min_count = 2,
-                        verbose = FALSE, threads = 1)
-#emb_lis   <- as.matrix(mod_lis)
+mod_lis <- word2vec(lis, dim = 50, iter = 5, min_count = 5,
+                    verbose = TRUE, threads = 4)
+emb_lis   <- as.matrix(mod_lis)
+dim(emb_lis)
+predict(mod_lis, c("people", "American"), type = "nearest")
 
-mod_txt <- word2vec(txt, dim = 15, iter = 20, split = c("[ \n]", "\n"), min_count = 2,
-                    verbose = FALSE, threads = 1)
+mod_txt <- word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
+                    verbose = TRUE, threads = 4)
 emb_txt   <- as.matrix(mod_txt)
-predict(mod_txt, c("citizen", "country"), type = "nearest")
+dim(emb_txt)
+predict(mod_txt, c("people", "American"), type = "nearest")
 
-n <- 100
-thread <- 3
-id <- 1
-floor((n / thread) * id)
-floor((n / thread) * (id + 1)) - 1
+
+microbenchmark::microbenchmark(
+    "lis" = word2vec(lis, dim = 50, iter = 5, min_count = 5,
+             verbose = FALSE, threads = 10),
+    "txt" = word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
+             verbose = FALSE, threads = 10),
+    times = 10
+)
+
