88075b72521f4c03f1b8cf969bcc9da8d6000fa4
Jan Wijffels <jwijffels@bnosac.be>
2023-10-04 00:12:26 +0200

    tests dependent on availability of packages

--- tests/train.R
+++ tests/train.R
@@ -1,30 +1,49 @@
-library(quanteda)
 library(word2vec)
 
-corp <- data_corpus_inaugural %>% 
-    corpus_reshape()
-toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE)
-lis <- as.list(toks)
-txt <- stringi::stri_c_list(lis, " ")
+if(require(quanteda, quietly = TRUE)){
+    library(quanteda) 
+    data("data_corpus_inaugural", package = "quanteda")
+    corp <- data_corpus_inaugural %>% 
+      corpus_reshape()
+    toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE)
+    lis <- as.list(toks)
+    txt <- stringi::stri_c_list(lis, " ")
+    x   <- as.character(data_corpus_inaugural)
+}else if(require(tokenizers.bpe, quietly = TRUE)){
+    library(tokenizers.bpe)
+    data(belgium_parliament, package = "tokenizers.bpe")
+    x <- belgium_parliament$text
+}else if(require(udpipe, quietly = TRUE)){
+    library(udpipe)
+    data(brussels_reviews, package = "udpipe")
+    x <- brussels_reviews$feedback
+}
+x   <- txt_clean_word2vec(x, ascii = TRUE, alpha = TRUE, tolower = TRUE, trim = TRUE)
 
-mod_lis <- word2vec(lis, dim = 50, iter = 5, min_count = 5,
-                    verbose = TRUE, threads = 4)
-emb_lis   <- as.matrix(mod_lis)
+# list-based approach
+lis <- strsplit(x, split = " ")
+set.seed(123456789)
+mod_lis <- word2vec(lis, dim = 50, iter = 20, min_count = 3, type = "cbow", lr = 0.01)
+emb_lis <- as.matrix(mod_lis)
 dim(emb_lis)
-predict(mod_lis, c("people", "American"), type = "nearest")
-
-mod_txt <- word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
-                    verbose = TRUE, threads = 4)
-emb_txt   <- as.matrix(mod_txt)
+# file-based approach
+set.seed(123456789)
+mod_txt <- word2vec(x, dim = 50, iter = 20, min_count = 3, type = "cbow", lr = 0.01)
+emb_txt <- as.matrix(mod_txt)
 dim(emb_txt)
-predict(mod_txt, c("people", "American"), type = "nearest")
 
 
-microbenchmark::microbenchmark(
-    "lis" = word2vec(lis, dim = 50, iter = 5, min_count = 5,
-             verbose = FALSE, threads = 10),
-    "txt" = word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
-             verbose = FALSE, threads = 10),
-    times = 10
-)
+## TEST equivalence
+setdiff(summary(mod_lis), summary(mod_txt))
+all.equal(summary(mod_lis), summary(mod_txt))
+all.equal(emb_lis, emb_txt)
 
+if(require(microbenchmark, quietly = TRUE)){
+    microbenchmark::microbenchmark(
+        "lis" = word2vec(lis, dim = 50, iter = 5, min_count = 5,
+                         verbose = FALSE, threads = 10),
+        "txt" = word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
+                         verbose = FALSE, threads = 10),
+        times = 10
+    )
+}
