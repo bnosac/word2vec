03065ee935f90f31fc6757b79b0b4030cd2c8423
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-11-21 07:39:50 +0900

    Add test file

---
+++ tests/train.R
@@ -0,0 +1,30 @@
+library(quanteda)
+library(word2vec)
+
+corp <- data_corpus_inaugural %>% 
+    corpus_reshape()
+toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE)
+lis <- as.list(toks)
+txt <- stringi::stri_c_list(lis, " ")
+
+mod_lis <- word2vec(lis, dim = 50, iter = 5, min_count = 5,
+                    verbose = TRUE, threads = 4)
+emb_lis   <- as.matrix(mod_lis)
+dim(emb_lis)
+predict(mod_lis, c("people", "American"), type = "nearest")
+
+mod_txt <- word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
+                    verbose = TRUE, threads = 4)
+emb_txt   <- as.matrix(mod_txt)
+dim(emb_txt)
+predict(mod_txt, c("people", "American"), type = "nearest")
+
+
+microbenchmark::microbenchmark(
+    "lis" = word2vec(lis, dim = 50, iter = 5, min_count = 5,
+                     verbose = FALSE, threads = 10),
+    "txt" = word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
+                     verbose = FALSE, threads = 10),
+    times = 10
+)
+
