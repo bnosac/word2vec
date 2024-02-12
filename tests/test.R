b15313d78d5fec887d2655472330248e4f1a7fe9
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-02-12 09:01:06 +0900

    Update

--- tests/test.R
+++ tests/test.R
@@ -3,29 +3,29 @@ library(word2vec)
 
 corp <- data_corpus_inaugural %>% 
     corpus_reshape()
-toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE)
-lis <- unclass(toks)
+toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE) %>% 
+    tokens_remove(stopwords(), padding = TRUE) %>% 
+    tokens_tolower()
+ndoc(toks)
 
-type <- types(toks)
-type[type %in% stopwords()] <- ""
-mod <- word2vec:::w2v_train(toks, type, verbose = TRUE)
+mod <- word2vec:::w2v_train(toks, types(toks), verbose = TRUE, size = 300, 
+                             iterations = 5, minWordFreq = 5)
 dim(as.matrix(mod))
+predict(mod, c("people", "american"), type = "nearest")
 
-mod2 <- word2vec:::w2v_train(unclass(toks)[1:10], types(toks), verbose = TRUE)
-dim(as.matrix(mod2))
+require(LSX)
+lss <- as.textmodel_lss(t(as.matrix(mod)), "good")
+head(coef(lss))
+tail(coef(lss))
 
+lis <- as.list(toks)
 mod_lis <- word2vec(lis, dim = 50, iter = 5, min_count = 5,
                     verbose = TRUE, threads = 4)
 emb_lis   <- as.matrix(mod_lis)
 dim(emb_lis)
-predict(mod_lis, c("people", "American"), type = "nearest")
+pred_lis <- predict(mod_lis, c("people", "American"), type = "nearest")
 
-mod_txt <- word2vec(txt, dim = 50, iter = 5, split = c("[ \n]", "\n"), min_count = 5,
-                    verbose = TRUE, threads = 4)
-emb_txt   <- as.matrix(mod_txt)
-dim(emb_txt)
-predict(mod_txt, c("people", "American"), type = "nearest")
-
+#saveRDS(mod_lis, "tests/word2vec_v04.RDS")
 
 microbenchmark::microbenchmark(
     "lis" = word2vec(lis, dim = 50, iter = 5, min_count = 5,
